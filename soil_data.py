ab7f39230106e28fea23be2b4fa8b36ccd125a23
Peter Norton <peter.norton@gazprom-mt.com>
2020-11-01 11:51:15 +0000

    sandbox.. working soil analysis

--- soil_data.py
+++ soil_data.py
@@ -4,26 +4,42 @@ import numpy as np
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+# ====================================================================
 # config
 filename = r'data\all_soil_data.csv'
 SITE = 'site'
-RANK_MEASURE = 'pH'
+INDEX = 'sample'
+
+# this is for ordering the sites
+RANK_MEASURE = 'C'
 rank_function = np.median  # any numpy aggregate function
 ASCENDING = True  # change the direction
-INDEX = 'sample'
-chart_columns = ['pH', 'Mass_g', 'N', 'C', 'Nmg', 'H', 'CN', 'wt_g']
-number_of_plots = 3
-chart_columns = chart_columns[:number_of_plots]
+
+# charting
+figure_height = 5
+aspect_ratio = 1.6
+all_chart_columns = ['pH', 'Mass_g', 'N', 'C', 'Nmg', 'H', 'CN', 'wt_g']
+chart_columns = ['pH', 'N', 'C', 'CN']
+chart_titles = {'pH': 'pH',
+                'N': 'Nitrogen',
+                'C': 'Carbon',
+                'CN': 'Carbon:Nitrogen ratio'}
+chart_config = 2, 2
+primary_site_keys = ['MSs', 'BW']
+figure_title = 'Fig 6: Variation in upper soil layer across all sites\n' \
+               f'(Mountsorrel and Button Wood then other sites ordered by {chart_titles[RANK_MEASURE]})'
+# ====================================================================
 
 # load the data into a dataframe and set the primary key
 df = pd.read_csv(filename)
 df.set_index(keys=INDEX, inplace=True)
 
+# only interested in a subset of the data
+mask = df['LAYER'] == 1
+df = df[mask]
+
 # get a unique list of all sites
 all_sites = list(df[SITE].unique())
-
-# there are two sites that should always be presented first
-primary_site_keys = ['MS', 'BW']
 
 # calculate the values we're going to rank by; this is all sites for now
 rank_values = df[[SITE, RANK_MEASURE]].groupby(by=SITE).agg(rank_function)
@@ -44,13 +60,20 @@ for k in all_site_list:
     site_dict[k] = df[mask]
 
 # chart it
-fig = Figure(figsize=(8, 6))
+fig = Figure(figsize=(figure_height * aspect_ratio, figure_height))
 FigureCanvas(fig)
 
+axes_dict = {}
 for i, c in enumerate(chart_columns):
-    ax = fig.add_subplot(1, number_of_plots, i+1)
-    ax.boxplot([df[c].values for df in site_dict.values()])
-    ax.set_xticklabels(site_dict.keys(), rotation=90)
+    axes_dict[c] = fig.add_subplot(*chart_config, i + 1)
+    axes_dict[c].boxplot([df[c].values for df in site_dict.values()])
+    axes_dict[c].set_title(chart_titles[c])
+    axes_dict[c].set_xticklabels(site_dict.keys(), rotation=90)
 
+for c in ['pH', 'N']:
+    axes_dict[c].set_xticklabels([])
+
+
+fig.suptitle(figure_title)
 fig.savefig('soil_boxplot')
 pass
